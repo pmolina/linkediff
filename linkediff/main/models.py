d7e810ef96eb91ff14739c5fb930337e623fc2a2
pmolina <patriciomolina@gmail.com>
2013-06-17 22:05:17 -0300

    Users should be able to have multiple pools, using db_index instead of unique

--- linkediff/main/models.py
+++ linkediff/main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Pool(models.Model):
-    owner = models.ForeignKey(User, unique=True)
+    owner = models.ForeignKey(User, db_index=True)
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
