89e25206a6d3e089a5fde923bdaa937ed84a4ec5
pmolina <patriciomolina@gmail.com>
2013-06-17 21:47:48 -0300

    (Unfinished) login controller and view

--- linkediff/linkediff/urls.py
+++ linkediff/linkediff/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'main.views.home', name='home'),
+    url(r'^login/', 'main.views.login', name='login'),
     # url(r'^linkediff/', include('linkediff.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- linkediff/main/views.py
+++ linkediff/main/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 def home(request):
     return render(request, 'home.html')
 
 def login(request):
-	return render(request, 'login.html')
+    if request.user.is_authenticated():
+        return redirect('home')
+    if request.POST:
+        if 'username' in request.POST and 'password' in request.POST:
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    auth_login(request, user)
+                    return redirect('home')
+                else:
+                    raise Exception('Account disabled')
+                    # Return a 'disabled account' error message
+            else:
+                return redirect('login')
+    return render(request, 'login.html')        
+
+def register(request):
+    return render(request, 'register.html')
+
+def logout(request):
+    auth_logout(request)
+    return redirect(home)
